0003cb7c033e5c1dce870b30404241537742a379
EwdsonT <ewdsontiago@gmail.com>
2026-03-25 22:01:45 -0300

    update variables

--- pages/lista-convidados.js
+++ pages/lista-convidados.js
@@ -4,8 +4,8 @@ export async function getServerSideProps() {
   const { Redis } = await import('@upstash/redis')
 
   const redis = new Redis({
-    url: process.env.UPSTASH_REDIS_REST_URL,
-    token: process.env.UPSTASH_REDIS_REST_TOKEN,
+    url: process.env.KV_REST_API_URL,
+    token: process.env.KV_REST_API_TOKEN,
   })
 
   let guests = []
